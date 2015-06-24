b02b11f28f1207a3f9df7b09610171998724c64b
SirWill <brieftaubenman@gmail.com>
2015-06-24 12:44:05 +0200

    Updated server files -> removed libaries, forge, ftb installer and added forge installer

--- _SERVER/ServerStart-Other.sh
+++ _SERVER/ServerStart-Other.sh
@@ -20,12 +20,13 @@ eula_false() {
 
 # run install script if MC server or launchwrapper s missing
 if [ ! -f minecraft_server.1.7.10.jar ]; then
-    echo "Missing required jars. Running install script!"
-    sh ./libraries/FTBInstall.sh
+    echo "Running installer!"
+    java -jar $forge-installer.jar --installServer
 fi
 
 # check if there is eula.txt and if it has correct content
 if [ -f eula.txt ] && eula_false ; then
+    printf "\033c"
     echo "Make sure to read eula.txt before playing!"
     echo "To exit press <enter>"
     read ignored
@@ -34,6 +35,7 @@ fi
 
 # inform user if eula.txt not found
 if [ ! -f eula.txt ]; then
+    printf "\033c"
     echo "Missing eula.txt. Startup will fail and eula.txt will be created"
     echo "Make sure to read eula.txt before playing!"
     echo "To continue press <enter>"
@@ -41,5 +43,5 @@ if [ ! -f eula.txt ]; then
 fi
 
 echo "Starting server"
-java -server -Xms$minMem -Xmx$maxMem -XX:PermSize=256m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $forge nogui
+java -server -Xms$minMem -Xmx$maxMem -XX:PermSize=256m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $forge-universal.jar nogui
 echo "Server process finished"
--- _SERVER/ServerStart-Windows.bat
+++ _SERVER/ServerStart-Windows.bat
@@ -10,8 +10,8 @@ for /f "delims=" %%x in (config.txt) do (set "%%x")
 
 
 if NOT EXIST minecraft_server.1.7.10.jar (
-    echo running install script!
-    call libraries/FTBInstall.bat
+    echo Running installer!
+    java -jar %forge%-installer.jar --installServer
 )
 
 REM Check if java in path
@@ -38,6 +38,7 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 if not exist eula.txt (
+    CLS
     echo Missing eula.txt. Startup will fail and eula.txt will be created
     echo Make sure to read eula.txt before playing!
     goto startserver
@@ -45,12 +46,13 @@ if not exist eula.txt (
 
 find "eula=false" eula.txt 1 > NUL 2>&1
 if %ERRORLEVEL% EQU 0 (
+     CLS
      echo Make sure to read eula.txt before playing! Exiting.
      pause
      exit /B
 )
 
 :startserver
-java -server -Xms%minMem% -Xmx%maxMem% -XX:PermSize=256m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar %forge% nogui
+java -server -Xms%minMem% -Xmx%maxMem% -XX:PermSize=256m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar %forge%-universal.jar nogui
 echo Server process finished
 pause
